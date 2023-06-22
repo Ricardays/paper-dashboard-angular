629e62280d1bacddc0a45947573525865d775e41
Ricardo Diaz <rdiaz@jacidi.com>
2023-06-22 01:44:30 -0400

    add: dashboard changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {catchError, Observable, take} from "rxjs";
+import {environment} from "../../../environments/environment";
 
 
 @Component({
@@ -14,196 +18,232 @@ export class DashboardComponent implements OnInit{
   public ctx;
   public chartColor;
   public chartEmail;
-  public chartHours;
+
+  protected headers: HttpHeaders;
+  env = environment;
+
+  pendingAppointments = 0;
+  activeMedics = 0;
+  createdPatients = 0;
+  cancelledAppointments = 0;
+
+  constructor(protected httpClient: HttpClient,
+              private router: Router,) { }
 
     ngOnInit(){
+
+      this.load();
+
       this.chartColor = "#FFFFFF";
 
-      this.canvas = document.getElementById("chartHours");
-      this.ctx = this.canvas.getContext("2d");
-
-      this.chartHours = new Chart(this.ctx, {
-        type: 'line',
-
-        data: {
-          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"],
-          datasets: [{
-              borderColor: "#6bd098",
-              backgroundColor: "#6bd098",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [300, 310, 316, 322, 330, 326, 333, 345, 338, 354]
-            },
-            {
-              borderColor: "#f17e5d",
-              backgroundColor: "#f17e5d",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [320, 340, 365, 360, 370, 385, 390, 384, 408, 420]
-            },
-            {
-              borderColor: "#fcc468",
-              backgroundColor: "#fcc468",
-              pointRadius: 0,
-              pointHoverRadius: 0,
-              borderWidth: 3,
-              data: [370, 394, 415, 409, 425, 445, 460, 450, 478, 484]
-            }
-          ]
-        },
-        options: {
-          legend: {
-            display: false
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
-              ticks: {
-                fontColor: "#9f9f9f",
-                beginAtZero: false,
-                maxTicksLimit: 5,
-                //padding: 20
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "#ccc",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent",
-                display: false,
-              },
-              ticks: {
-                padding: 20,
-                fontColor: "#9f9f9f"
-              }
-            }]
-          },
-        }
-      });
-
-
-      this.canvas = document.getElementById("chartEmail");
-      this.ctx = this.canvas.getContext("2d");
-      this.chartEmail = new Chart(this.ctx, {
-        type: 'pie',
-        data: {
-          labels: [1, 2, 3],
-          datasets: [{
-            label: "Emails",
-            pointRadius: 0,
-            pointHoverRadius: 0,
-            backgroundColor: [
-              '#e3e3e3',
-              '#4acccd',
-              '#fcc468',
-              '#ef8157'
-            ],
-            borderWidth: 0,
-            data: [342, 480, 530, 120]
-          }]
-        },
-
-        options: {
-
-          legend: {
-            display: false
-          },
-
-          pieceLabel: {
-            render: 'percentage',
-            fontColor: ['white'],
-            precision: 2
-          },
-
-          tooltips: {
-            enabled: false
-          },
-
-          scales: {
-            yAxes: [{
-
-              ticks: {
-                display: false
-              },
-              gridLines: {
-                drawBorder: false,
-                zeroLineColor: "transparent",
-                color: 'rgba(255,255,255,0.05)'
-              }
-
-            }],
-
-            xAxes: [{
-              barPercentage: 1.6,
-              gridLines: {
-                drawBorder: false,
-                color: 'rgba(255,255,255,0.1)',
-                zeroLineColor: "transparent"
-              },
-              ticks: {
-                display: false,
-              }
-            }]
-          },
-        }
-      });
-
-      var speedCanvas = document.getElementById("speedChart");
-
-      var dataFirst = {
-        data: [0, 19, 15, 20, 30, 40, 40, 50, 25, 30, 50, 70],
-        fill: false,
-        borderColor: '#fbc658',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#fbc658',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8,
-      };
-
-      var dataSecond = {
-        data: [0, 5, 10, 12, 20, 27, 30, 34, 42, 45, 55, 63],
-        fill: false,
-        borderColor: '#51CACF',
-        backgroundColor: 'transparent',
-        pointBorderColor: '#51CACF',
-        pointRadius: 4,
-        pointHoverRadius: 4,
-        pointBorderWidth: 8
-      };
-
-      var speedData = {
-        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-        datasets: [dataFirst, dataSecond]
-      };
-
-      var chartOptions = {
-        legend: {
-          display: false,
-          position: 'top'
-        }
-      };
-
-      var lineChart = new Chart(speedCanvas, {
-        type: 'line',
-        hover: false,
-        data: speedData,
-        options: chartOptions
-      });
     }
+
+  load(): void {
+    this.httpClient.get(this.env.urlApi + '/appointment/dashboard/pending', {headers: this.headers})
+      .pipe(
+        take(1),
+        catchError(err => this.handleErr(err))
+      ).subscribe(
+      (resp) => {
+        this.pendingAppointments = resp;
+      }
+    );
+
+    this.httpClient.get(this.env.urlApi + '/medic/dashboard/active', {headers: this.headers})
+      .pipe(
+        take(1),
+        catchError(err => this.handleErr(err))
+      ).subscribe(
+      (resp) => {
+        this.activeMedics = resp;
+      }
+    );
+
+    this.httpClient.get(this.env.urlApi + '/patient/dashboard/created', {headers: this.headers})
+      .pipe(
+        take(1),
+        catchError(err => this.handleErr(err))
+      ).subscribe(
+      (resp) => {
+        this.createdPatients = resp;
+      }
+    );
+
+    this.httpClient.get(this.env.urlApi + '/appointment/dashboard/cancelled', {headers: this.headers})
+      .pipe(
+        take(1),
+        catchError(err => this.handleErr(err))
+      ).subscribe(
+      (resp) => {
+        this.cancelledAppointments = resp;
+      }
+    )
+
+    this.httpClient.get(this.env.urlApi + '/appointment/dashboard/graph', {headers: this.headers})
+      .pipe(
+        take(1),
+        catchError(err => this.handleErr(err))
+      ).subscribe(
+      (resp) => {
+        let graphData = [
+          resp.Pendiente?resp.Pendiente:0 ,
+          resp.Confirmado?resp.Confirmado:0 ,
+          resp.Completado?resp.Completado:0 ,
+          resp.Cancelado?resp.Cancelado:0
+        ]
+
+        this.canvas = document.getElementById("chartEmail");
+        this.ctx = this.canvas.getContext("2d");
+        this.chartEmail = new Chart(this.ctx, {
+          type: 'pie',
+          data: {
+            labels: [1, 2, 3],
+            datasets: [{
+              label: "Emails",
+              pointRadius: 0,
+              pointHoverRadius: 0,
+              backgroundColor: [
+                '#e3e3e3',
+                '#4acccd',
+                '#fcc468',
+                '#ef8157'
+              ],
+              borderWidth: 0,
+              data: graphData
+            }]
+          },
+
+          options: {
+
+            legend: {
+              display: false
+            },
+
+            pieceLabel: {
+              render: 'percentage',
+              fontColor: ['white'],
+              precision: 2
+            },
+
+            tooltips: {
+              enabled: false
+            },
+
+            scales: {
+              yAxes: [{
+
+                ticks: {
+                  display: false
+                },
+                gridLines: {
+                  drawBorder: false,
+                  zeroLineColor: "transparent",
+                  color: 'rgba(255,255,255,0.05)'
+                }
+
+              }],
+
+              xAxes: [{
+                barPercentage: 1.6,
+                gridLines: {
+                  drawBorder: false,
+                  color: 'rgba(255,255,255,0.1)',
+                  zeroLineColor: "transparent"
+                },
+                ticks: {
+                  display: false,
+                }
+              }]
+            },
+          }
+        });
+
+        this.httpClient.get(this.env.urlApi + '/appointment/dashboard/linegraph', {headers: this.headers})
+          .pipe(
+            take(1),
+            catchError(err => this.handleErr(err))
+          ).subscribe(
+          (resp) => {
+            console.log(resp)
+            let graphActual = [
+              resp.actual?.JANUARY? resp.actual?.JANUARY : 0,
+              resp.actual?.FEBRUARY? resp.actual?.FEBRUARY : 0,
+              resp.actual?.MARCH? resp.actual?.MARCH : 0,
+              resp.actual?.APRIL? resp.actual?.APRIL : 0,
+              resp.actual?.MAY? resp.actual?.MAY : 0,
+              resp.actual?.JUNE? resp.actual?.JUNE : 0,
+              resp.actual?.JULY? resp.actual?.JULY : 0,
+              resp.actual?.AUGUST? resp.actual?.AUGUST : 0,
+              resp.actual?.SEPTEMBER? resp.actual?.SEPTEMBER : 0,
+              resp.actual?.OCTOBER? resp.actual?.OCTOBER : 0,
+              resp.actual?.NOVEMBER? resp.actual?.NOVEMBER : 0,
+              resp.actual?.DECEMBER? resp.actual?.DECEMBER : 0,
+            ];
+
+            let graphPast = [
+              resp.past?.JANUARY? resp.past?.JANUARY : 0,
+              resp.past?.FEBRUARY? resp.past?.FEBRUARY : 0,
+              resp.past?.MARCH? resp.past?.MARCH : 0,
+              resp.past?.APRIL? resp.past?.APRIL : 0,
+              resp.past?.MAY? resp.past?.MAY : 0,
+              resp.past?.JUNE? resp.past?.JUNE : 0,
+              resp.past?.JULY? resp.past?.JULY : 0,
+              resp.past?.AUGUST? resp.past?.AUGUST : 0,
+              resp.past?.SEPTEMBER? resp.past?.SEPTEMBER : 0,
+              resp.past?.OCTOBER? resp.past?.OCTOBER : 0,
+              resp.past?.NOVEMBER? resp.past?.NOVEMBER : 0,
+              resp.past?.DECEMBER? resp.past?.DECEMBER : 0,
+            ];
+
+            var speedCanvas = document.getElementById("speedChart");
+
+            var dataFirst = {
+              data: graphActual,
+              fill: false,
+              borderColor: '#fbc658',
+              backgroundColor: 'transparent',
+              pointBorderColor: '#fbc658',
+              pointRadius: 4,
+              pointHoverRadius: 4,
+              pointBorderWidth: 8,
+            };
+
+            var dataSecond = {
+              data: graphPast,
+              fill: false,
+              borderColor: '#51CACF',
+              backgroundColor: 'transparent',
+              pointBorderColor: '#51CACF',
+              pointRadius: 4,
+              pointHoverRadius: 4,
+              pointBorderWidth: 8
+            };
+
+            var speedData = {
+              labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+              datasets: [dataFirst, dataSecond]
+            };
+
+            var chartOptions = {
+              legend: {
+                display: false,
+                position: 'top'
+              }
+            };
+
+            var lineChart = new Chart(speedCanvas, {
+              type: 'line',
+              hover: false,
+              data: speedData,
+              options: chartOptions
+            });
+          });
+      }
+    )
+  }
+
+  protected handleErr(err): Observable<any> {
+    console.error('API Error:', err);
+    throw err;
+  }
 }
